7d2165aa7d458c6219d1b9f3a7f6f26189a4a33a
MatheusHenrique564 <matheus.henrique.senai564@gmail.com>
2024-05-08 17:14:30 -0300

    Created models in pages calcWinner and calcEXP

--- src/models/calculateExp.js
+++ src/models/calculateExp.js
@@ -1,1 +1,9 @@
 const pool = require("../config/dbConfig")
+
+async function calculateExp (anti_hero) {
+    if (anti_hero.experience >= 100) {
+        await pool.query('UPDATE anti_heroes SET lvl = $1, experience = $2 WHERE id = $3', [anti_hero.lvl + 1, 0, anti_hero.id]);
+    }
+}
+
+module.exports = calculateExp;
--- src/models/calculateWinner.js
+++ src/models/calculateWinner.js
@@ -1,1 +1,18 @@
 const pool = require("../config/dbConfig")
+
+async function calculateWinner (anti_hero1, anti_hero2) {
+    const anti_hero1_power = anti_hero1.health * anti_hero1.attack;
+    const anti_hero2_power = anti_hero2.health * anti_hero2.attack;
+
+    if (anti_hero1_power > anti_hero2_power) {
+        await pool.query('UPDATE anti_heroes SET experience = $1 WHERE id = $2', [anti_hero1.experience + 25, anti_hero1.id]);
+        await pool.query('UPDATE anti_heroes SET experience = $1 WHERE id = $2', [anti_hero2.experience + 10, anti_hero2.id]);
+        return anti_hero1;
+    } else {
+        await pool.query('UPDATE anti_heroes SET experience = $1 WHERE id = $2', [anti_hero1.experience + 10, anti_hero1.id]);
+        await pool.query('UPDATE anti_heroes SET experience = $1 WHERE id = $2', [anti_hero2.experience + 25, anti_hero2.id]);
+        return anti_hero2;
+    }
+}
+
+module.exports = calculateWinner;
